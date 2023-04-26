3e3e7a08a912801f6b34a73a18a2b04e44177783
AliAlSubhi <hopeofoman1@gmail.com>
2023-04-26 20:40:20 +0400

    getAllJobSeekers Apis getAll (++check)

--- src/main/java/com/alialsubhi/demoProject1/Controllers/JobSeekerControllers.java
+++ src/main/java/com/alialsubhi/demoProject1/Controllers/JobSeekerControllers.java
@@ -1,6 +1,8 @@
 package com.alialsubhi.demoProject1.Controllers;
 
 import com.alialsubhi.demoProject1.Models.Job;
+import com.alialsubhi.demoProject1.Models.JobSeeker;
+import com.alialsubhi.demoProject1.Services.JobSeekerServices;
 import com.alialsubhi.demoProject1.Services.JobServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "jobs")
-public class JobControllers {
+@RequestMapping(value = "job seekers")
+public class JobSeekerControllers {
 
     @Autowired
-    JobServices jobServices;
+    JobSeekerServices jobSeekerServices;
     @GetMapping(value = "getAll")
-    public List<Job> getAllJobs(){
-        return  jobServices.getAllJobs();
+    public List<JobSeeker> getAllJobSeekers(){
+        return  jobSeekerServices.getAllJobSeekers();
     }
 }
